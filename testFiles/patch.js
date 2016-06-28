a1dd63fdf506f733301d9de5c89e8ba012360ae2
xuzichao03 <xuzichao03@bytedance.com>
2016-06-28 17:27:59 +0800

    TTLiveMainViewController

--- testFiles/patch.js
+++ testFiles/patch.js
@@ -2,8 +2,9 @@
 
 defineClass('TTLiveMainViewController', {
     ttLiveFakeNavigationBarEllipsisBtnClicked:function() {
-        var model = self.topInfoModel;
-        self.setUpShareFollowViewWithModel(model);
-        self.shareFollowView.showAnimate(true);
+            var model = self.topInfoModel;
+            self.setUpShareFollowViewWithModel(model);
+            var shareView = self.shareFollowView;
+            shareView.showAnimate(true);
     }
 });
